35d67b283471f6299e6c0083eaa4a909a274215c
Curing0 <AnimesStudioBrasil@gmail.com>
2018-12-22 10:38:09 -0200

    Add files via upload

---
+++ setup-arch.sh
@@ -0,0 +1,7 @@
+sudo pacman -S figlet
+figlet Install
+sleep 0.5
+sudo pacman -S python
+sudo pacman -S python-pip
+sudo pip install youtube-dl
+echo "[Instalação conluída]"
---
+++ vdown.py
@@ -0,0 +1,64 @@
+from tkinter import *
+import os, os.path
+
+if os.path.exists("~/videos"):
+    print("Verificando existencia da pasta videos")
+else:
+    print("pasta videos nao existe\n[Criando..]")
+    os.system("mkdir ~/videos")
+if os.path.exists("~/musics"):
+    print("Diretório existe")
+else:
+    os.system("mkdir ~/musics")
+
+def vid():  
+  a = l.get()
+  if (a == ""):
+    con['fg']= "red"
+    con['text']= "O link está vazio!"
+  else:
+    os.system("youtube-dl {} -f mp4 -o \"~/videos/%(title)s.%(ext)s\"".format(a))
+    con['fg']= "green"
+    con['text']= "Arquivo baixado!"
+def mus():
+  a = l.get()
+  if (a == ""):
+    con['fg']= "red"
+    con['text'] = "O link está vazio!"
+  else:
+    os.system("youtube-dl {} --extract-audio --audio-format mp3 -o \"~/musics/%(title)s.%(ext)s\"".format(a))
+    con['text']=" "
+    con['fg']= "green"
+    con['text']= "Arquivo baixado!"
+j = Tk()
+j.title("vdown(Beta) by: curing0")
+j.geometry("500x250")
+
+tp = Label(j, text="Video Downloader", font="Verdana 20")
+tp.pack()
+
+tl = Label(j, text="Insira o link do video abaixo:")
+tl.pack()
+
+l = Entry()
+l.pack()
+
+bv = Button(j, text="Download Video", command = vid)
+bv.pack()
+
+bm = Button(j, text="Download Music", command = mus)
+bm.pack()
+
+lem = Label(j, text="OBS: Os arquivos são salvos automaticamente em ~/videos/ ou ~/musics")
+lem.pack()
+
+con = Label(j, text="")
+con.pack()
+
+by = Label(j, text="My Telegram: @Curing0", fg="blue")
+git = Label(j, text="My Github: https://github.com/curing0")
+don = Label(j, text="Donate: 3H5jubwe2HraBz5LmLyUtWE1QuUL4VJSJi", fg="yellow")
+by.pack()
+git.pack()
+don.pack()
+j.mainloop()
