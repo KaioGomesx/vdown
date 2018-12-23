1a7f8ae49ad0b0f0d37443dcebcbcad2704cbb98
Curing0 <AnimesStudioBrasil@gmail.com>
2018-12-23 03:35:16 -0200

    Update setup-mint.sh

--- setup-mint.sh
+++ setup-mint.sh
@@ -7,5 +7,5 @@ sudo apt-get install tk
 sudo pip install youtube-dl
 sudo apt-get install ffmpeg
 sudo cp vdown.py /usr/bin
-sudo cp duck.ico /usr/share/icons/
+sudo cp duck.gif /usr/share/icons/
 sudo echo -e " \n[Desktop Entry]\nType=Application\nName=Vdown\nExec=python3 /usr/bin/vdown.py\nIcon=/usr/share/icons/duck.ico" > /home/$USER/Área\ de\ Trabalho/Vdown.desktop && sleep 0.5 && chmod +x /home/$USER/Área\ de\ Trabalho/Vdown*
