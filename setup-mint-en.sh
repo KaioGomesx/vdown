1d8aabde0a9b9c608f8e5f2d5bac7bf52b7f87b5
Curing0 <AnimesStudioBrasil@gmail.com>
2018-12-24 06:07:29 -0200

    Update setup-mint-en.sh

--- setup-mint-en.sh
+++ setup-mint-en.sh
@@ -10,5 +10,4 @@ sudo cp vdown.py /usr/bin
 sudo cp duck.gif /usr/share/icons/
 sudo mkdir /usr/share/vdown/
 sudo echo -e " \n[Desktop Entry]\nType=Application\nName=Vdown\nExec=python3 /usr/bin/vdown.py\nIcon=/usr/share/icons/duck.ico" > /home/$USER/Desktop/Vdown.desktop && sleep 0.5 && chmod +x /home/$USER/Desktop/Vdown*
-sudo echo -e " \n[Desktop Entry]\nType=Application\nName=Vdown\nExec=python3 /usr/bin/vdown.py\nIcon=/usr/share/icons/duck.ico" > /usr/share/applications/Vdown.desktop && sleep 0.5 && sudo chmod +x /usr/share/applications/Vdown*
 sudo python3 language.py
