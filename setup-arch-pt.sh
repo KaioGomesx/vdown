8d3a0485345124fb47afeda3d94bfb672c35fa48
Curing0 <AnimesStudioBrasil@gmail.com>
2018-12-24 06:03:04 -0200

    Add files via upload

---
+++ setup-arch-pt.sh
@@ -0,0 +1,15 @@
+sudo pacman -S figlet
+figlet Install
+sleep 0.5
+sudo pacman -S python
+sudo pacman -S python-pip
+sudo pacman -S tk
+sudo pip install youtube-dl
+sudo pacman -S ffmpeg
+sudo cp vdown.py /usr/bin/
+sudo cp duck.ico /usr/share/icons/
+sudo mkdir /usr/share/vdown/
+sudo echo -e " \n[Desktop Entry]\nType=Application\nName=Vdown\nExec=python3 /usr/bin/vdown.py\nIcon=/usr/share/icons/duck.ico" > /home/$USER/Área\ de\ Trabalho/Vdown.desktop && sleep 0.5 && sudo chmod +x /home/$USER/Área\ de\ Trabalho/Vdown*
+sudo echo -e " \n[Desktop Entry]\nType=Application\nName=Vdown\nExec=python3 /usr/bin/vdown.py\nIcon=/usr/share/icons/duck.ico" > /usr/share/applications/Vdown.desktop && sleep 0.5 && sudo chmod +x /usr/share/applications/Vdown*
+echo "[Instalação conluída]"
+sudo python3 language.py
